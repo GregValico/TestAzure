61485a8d581f78d427a3077f4de440cb08a0281c
GregValico <GregValico@gmail.com>
2022-09-12 15:35:53 +0000

    more changes

--- Test1/vmss.tf
+++ Test1/vmss.tf
@@ -23,7 +23,7 @@ resource "azurerm_subnet" "vmss" {
  name                 = "vmss-subnet"
  resource_group_name  = azurerm_resource_group.vmss.name
  virtual_network_name = azurerm_virtual_network.vmss.name
- address_prefixes       = "10.0.2.0/24"
+ address_prefixes       = ["10.0.2.0/24"]
 }
 
 resource "azurerm_public_ip" "vmss" {
@@ -60,13 +60,12 @@ resource "azurerm_lb_probe" "vmss" {
 }
 
 resource "azurerm_lb_rule" "lbnatrule" {
-   resource_group_name            = azurerm_resource_group.vmss.name
    loadbalancer_id                = azurerm_lb.vmss.id
    name                           = "http"
    protocol                       = "Tcp"
    frontend_port                  = var.application_port
    backend_port                   = var.application_port
-   backend_address_pool_id        = azurerm_lb_backend_address_pool.bpepool.id
+   backend_address_pool_ids        = azurerm_lb_backend_address_pool.bpepool.id
    frontend_ip_configuration_name = "PublicIPAddress"
    probe_id                       = azurerm_lb_probe.vmss.id
 }
